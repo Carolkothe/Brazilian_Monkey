9c64732de8ff49e8ebdf21aa490d2492f87bf74a
nmohellibi <40435523+nmohellibi@users.noreply.github.com>
2021-06-17 17:36:10 +0200

    Update picrust.sh
    Add genericity to the script with DATA_DIR and OUTPUT_DIR variables.

--- Sources/picrust.sh
+++ Sources/picrust.sh
@@ -1,1 +1,3 @@
-conda activate picrust2-2.3.0_b && picrust2_pipeline.py -s seqs.fna -i table.biom -o picrust2_out_pipeline -p 1
+DATA_DIR="C:/Users/myUser/data"
+OUTPUT_DIR="C:/Users/myUser/output"
+conda activate picrust2-2.3.0_b && picrust2_pipeline.py -s $DATA_DIR/seqs.fna -i $DATA_DIR/table.biom -o $OUTPUT_DIR/picrust2_out_pipeline -p 1
